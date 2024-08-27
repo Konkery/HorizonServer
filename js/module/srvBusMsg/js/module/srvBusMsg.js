d591c6515b8320585667ecf1615efd4e9aef0bc0
Nikita <nikita.umnov@mas-tech.ru>
2024-08-27 18:06:32 +0400

    feat: добавлены поля type и value feat: добавлен экспорт констант refactor: незначительные изменения в структуре кода

--- js/module/srvBusMsg/js/module/srvBusMsg.js
+++ js/module/srvBusMsg/js/module/srvBusMsg.js
@@ -1,30 +1,62 @@
+/** КОНСТАНТЫ */
+const MSG_TYPE_EVENT    = 'event';     // стандартное сообщение, не требующее ответа
+const MSG_TYPE_REQUEST  = 'req';         // запрос, ответ на который будет ожидаться посредством async-await
+const MSG_TYPE_RESPONSE = 'res';        // ответ
+const MSG_TYPE_LIST = [MSG_TYPE_EVENT, MSG_TYPE_REQUEST, MSG_TYPE_RESPONSE]; 
+/************ */
+const generateHash = () => new Date().getTime()*Math.random();
 
-const generateHash = new Date().getTime()*Math.random();
-
-const getStrOrErr = (key, val) => {
-    return (typeof val == 'string') ? val : new Error(`${key} must be a string`);
-}
-
-const getArrOrErr = (key, arr) => {
-    return Array.isArray(arr) ? arr : new Error(`${key} must be an array`);
-}
-
+/**
+ * @typedef TypeBusMsgConstructor
+ * @property {string} com
+ * @property {[]} arg
+ * @property {} value 
+ */
 /**
  * @class
  * Сообщение, предназначенное для передачи по шине фреймворка
  */
-class BusMsg_S {
-    constructor({ topic, payload: { com, arg=[], source, destinations } }) {
-        this.topic =  getStrOrErr('topic', topic),
+class ClassBusMsg_S {
+    /**
+     * @constructor
+     * @param {TypeBusMsgConstructor} _msg 
+     */
+    constructor({ topic, payload: { com, arg=[], value, source, destinations, type } }) {
+        this.topic =  this.#GetStrOrErr('topic', topic),
         this.payload = {
             timestamp: new Date().getTime(),
-            hash:         generateHash(),                //// TODO: использовать библиотечную функцию
-            com:          getStrOrErr('com', com),
-            arg:          getArrOrErr('arg', arg),
-            source:       getStrOrErr('source', source),
-            destinations: getArrOrErr('destinations', destinations),
+            hash:         generateHash(),                // TODO: использовать библиотечную функцию
+            com:          this.#GetStrOrErr('com', com),
+            arg:          this.#GetArrOrErr('arg', arg),
+            value:        value,                        //TODO: указать тип value
+            source:       this.#GetStrOrErr('source', source),
+            destinations: this.#GetArrOrErr('destinations', destinations),
+            type:         this.#GetType(type)
         }
     }
+    /** утилитарные методы для работы с вх.данными */
+    #GetStrOrErr = (key, val) => {
+        return (typeof val == 'string') ? val : new Error(`${key} must be a string`);
+    }
+
+    #GetArrOrErr = (key, arr) => {
+        return Array.isArray(arr) ? arr : new Error(`${key} must be an array`);
+    }
+
+    #GetType = typeName => {
+        if (MSG_TYPE_LIST.includes(typeName))
+            return typeName;
+        return MSG_TYPE_EVENT;
+    }
+    /********************* */
 }
 
-module.exports = { BusMsg: BusMsg_S, HrzBusReq };
+module.exports = { 
+    ClassBusMsg_S, 
+    constants: { 
+        MSG_TYPE_DEFAULT: MSG_TYPE_EVENT, 
+        MSG_TYPE_REQUEST, 
+        MSG_TYPE_RESPONSE, 
+        MSG_TYPE_LIST
+    }
+};
