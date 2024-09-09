d95c23f1685da21fea977fab2861d36950f4be52
Nikita <nikita.umnov@mas-tech.ru>
2024-09-09 12:08:47 +0400

    feat: поля source, dest, demandRes,  hash внесены в metadata feat: свойство type сведено до двух вариантов: req и res doc: доработана документация вх. параметров класса

--- js/module/srvBusMsg/js/module/srvBusMsg.js
+++ js/module/srvBusMsg/js/module/srvBusMsg.js
@@ -1,16 +1,20 @@
 /** КОНСТАНТЫ */
-const MSG_TYPE_EVENT    = 'event';     // стандартное сообщение, не требующее ответа
-const MSG_TYPE_REQUEST  = 'req';         // запрос, ответ на который будет ожидаться посредством async-await
+const MSG_TYPE_REQUEST  = 'req';        // запрос
 const MSG_TYPE_RESPONSE = 'res';        // ответ
-const MSG_TYPE_LIST = [MSG_TYPE_EVENT, MSG_TYPE_REQUEST, MSG_TYPE_RESPONSE]; 
+const MSG_TYPE_LIST = [MSG_TYPE_REQUEST, MSG_TYPE_RESPONSE]; 
 /************ */
-const generateHash = () => new Date().getTime()*Math.random();
+const generateHash = () => Math.trunc(new Date().getTime()*Math.random());
 
 /**
  * @typedef TypeBusMsgConstructor
  * @property {string} com
- * @property {[]} arg
- * @property {} value 
+ * @property {[any]} arg
+ * @property {[any]} value
+ * @property {string} service - имя службы, которая отправила сообщение; 
+ * @property {string} source 
+ * @property {string} dest - имя источника, которому предназначается контент сообщения
+ * @property {boolean} demandRes - флаг того, требует ли сообщение ответ, который будет ожидаться посредством async-await
+ * @property {string|number} [hash] - хэш сообщения, генерируется автоматически либо передается в конструктор; во втором случае сообщению присваивается type = 'res'
  */
 /**
  * @class
@@ -21,29 +25,30 @@ class ClassBusMsg_S {
      * @constructor
      * @param {TypeBusMsgConstructor} _msg 
      */
-    constructor({ topic, payload: { com, arg=[], value, source, destinations, type } }) {
-        this.topic =  this.#GetStrOrErr('topic', topic),
-        this.payload = {
-            timestamp: new Date().getTime(),
-            hash:         generateHash(),                // TODO: использовать библиотечную функцию
-            com:          this.#GetStrOrErr('com', com),
-            arg:          this.#GetArrOrErr('arg', arg),
-            value:        value,                        //TODO: указать тип value
-            source:       this.#GetStrOrErr('source', source),
-            destinations: this.#GetArrOrErr('destinations', destinations),
-            type:         this.#GetType(type)
-        }
+    constructor({ com, arg=[], value=[], source, service, dest, demandRes=false,  hash }) {
+        this.timestamp = new Date().getTime(),
+        this.metadata = {
+            hash: hash ?? generateHash(),                // TODO: использовать библиотечную функцию
+            type: hash ? MSG_TYPE_RESPONSE : MSG_TYPE_REQUEST,
+            demandRes: Boolean(demandRes),
+            service: this.#GetStrOrErr('service', service),
+            source: this.#GetStrOrErr('source', source),
+            dest
+        },
+        this.com = this.#GetStrOrErr('com', com),
+        this.arg = this.#GetArrOrErr('arg', arg),
+        this.value = this.#GetArrOrErr('value', value)    
     }
     /** утилитарные методы для работы с вх.данными */
-    #GetStrOrErr = (key, val) => {
+    #GetStrOrErr(key, val) {
         return (typeof val == 'string') ? val : new Error(`${key} must be a string`);
     }
 
-    #GetArrOrErr = (key, arr) => {
+    #GetArrOrErr(key, arr) {
         return Array.isArray(arr) ? arr : new Error(`${key} must be an array`);
     }
 
-    #GetType = typeName => {
+    #GetType(typeName) {
         if (MSG_TYPE_LIST.includes(typeName))
             return typeName;
         return MSG_TYPE_EVENT;
@@ -54,7 +59,6 @@ class ClassBusMsg_S {
 module.exports = { 
     ClassBusMsg_S, 
     constants: { 
-        MSG_TYPE_DEFAULT: MSG_TYPE_EVENT, 
         MSG_TYPE_REQUEST, 
         MSG_TYPE_RESPONSE, 
         MSG_TYPE_LIST
