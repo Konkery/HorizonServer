73e11765dba181e89349212cedb9c705f2c0fbfe
Nikita <nikita.umnov@mas-tech.ru>
2024-09-16 09:59:50 +0400

    fix: актуализировано имя службы и параметры конструктора feat: "устаканены" имена событий:     'proxywsc-send' - отправка сообщения от службы (для отправки на wsc);     'proxywsc-get-msg' - прием сообщения от wsc и его распаковка;     'wsc-send' - отправка сообщения на wsc; feat: переработан метод HandlerEvents_proxywsc_send:     1. распаковка сообщения     2. [если сообщение требует ответ] сохранение хэша по resCom (имя ответной команды)     3. переупаковка и отправка сообщения на wsc feat: переработан метод HandlerEvents_proxywsc_get_msg (ex Receive):     1. распаковка сообщения     2. поиск сохраненного хэша     3. отправка на шину сообщения с arg: source_name, value: [сообщение с plc], com: (сообщение_с_plc).com     метод более не проверяет отвечает за формирование сообщения raw-data на измерительный канал fix: убрана работа с заглушкой LHP, которая обеспечивала форматирование по старому формату

--- js/module/ProxyWSSrv/js/module/srvProxyWS.js
+++ js/module/ProxyWSSrv/js/module/srvProxyWS.js
@@ -1,61 +1,16 @@
-const ClassBaseService_S = require('srvService');
-//const ClassBaseService_S = class {};
-const EVENT_INIT = 'all-init';
-const EVENT_REGISTER = 'register';
-const EVENT_WSC_RECEIVE     = 'pwsc-receive';
-const EVENT_PWS_CREATED     = 'pwsc-created';
-const EVENT_CONNS_DONE      = 'proc-connections-done';
-const EVENT_WSC_MSG_RETURN  = 'wsc-msg-return';
-const EVENT_PWSC_MSG_RETURN = 'pwsc-msg-return';
-const EVENT_PWSC_SEND       = 'pwsc-send';
+const { hash } = require('crypto');
+const ClassBaseService_S = require('./srvService');
 
-/** Новые команды */
-const COM_PWSC_SEND = 'send';
-const COM_PWSC_RECEIVE = 'receive';
+const KEEP_ALIVE_HASH = 5000;
 
-const BUS_NAMES_LIST = ['sysBus', 'logBus', 'lhpBus'];
+const COM_PWSC_SEND    = 'proxywsc-send';
+const COM_PWSC_GET_MSG = 'proxywsc-get-msg';
+const COM_WSC_SEND = 'wsc-send';
 
-const LHP = {
-    /**
-     * @typedef Command
-     * @property {string} com - строковое представление команды
-     * @property {[any]} arg - аргументы
-     */
-    /**
-     * @method
-     * Метод формирует LHP-пакет из полученной команды 
-     * @param {Command} com
-     * @returns {object}
-     */
-    Pack(com) {
-        // TODO: реализовать получение ID и TimeStamp
-        const msg = { 
-            "TimeStamp": 122465768,
-            "MetaData": {                   
-                "Type":     "server",
-                "ID":       "MAS-M008",
-                "Command":  [ com ]
-            },
-            "Value": ""
-        };
-        msg.MetaData.CRC = Math.random()*1111    //расчет чексуммы
-        return msg;
-    },
-    Unpack(_data, _sourceId) {
-        let obj = null;
-        try {
-            obj = JSON.parse(_data);
-        } catch (e) {
-            throw new err('Incorrect JSON data');
-        }
-        
-        let meta_crc = obj.MetaData.CRC;    //чексумма, полученная из пакета
-        delete obj.MetaData.CRC;
-        let service = obj.MetaData.RegServices;
-        let comObj = obj.MetaData[service];
-        return comObj;
-    }
-}
+const EVENT_ON_LIST_LHPBUS = [COM_PWSC_SEND, COM_PWSC_GET_MSG];
+const EVENT_EMIT_LIST_WSC = [COM_WSC_SEND];
+
+const BUS_NAME_LIST = ['sysBus', 'logBus', 'lhpBus'];
 
 /**
  * @class является придатком WS Client и реализует 
@@ -64,89 +19,114 @@ const LHP = {
  */
 class ClassProxyWSClient extends ClassBaseService_S {
     #_SourcesState;
+    #_RequestList = {};
     /**
      * @constructor
      * @param {[ClassBus_S]} busList - список шин, созданных в проекте
      */
-    constructor({ busList, node }) {
+    constructor({ _busList, _node }) {
         // передача в супер-конструктор имени службы и списка требуемых шин
-        super({ name: 'ProxyWSClient', busNamesList: BUS_NAMES_LIST, busList, node });
-        /**
-         * @event
-         * Получение LHP пакета от WS клиента
-         * @param {string} msg - JSON-строка
-         * @param {string} sourceKey - первоначальный идентификатор соединения
-         */
-        this._BusList.sysBus.on(EVENT_WSC_MSG_RETURN, this.#Receive.bind(this));
-
-        /**
-         * @event
-         * Запрос на отправку данных по WS соединению
-         * @param {object} command 
-         * @param {string} sourceName - имя соединения
-         */
-        this._BusList.sysBus.on(EVENT_PWSC_SEND, (command, sourceName) => {
-            console.log(`PWSC | ${EVENT_PWSC_SEND}    ${command}`);
-            const msg = LHP.Pack(command);
-            this.#SendOnWSC(msg, sourceName);
-        });
+        super({ _name: 'proxywsc', _busNameList: BUS_NAME_LIST, _busList, _node });
+        this.FillEventOnList('lhpBus', EVENT_ON_LIST_LHPBUS);
     }
-
-    /**
-     * @typedef InitOpts
-     * @property {HrzBus} sysBus
-     * @property {object} SourcesInfo
-     */
     /**
      * @method
-     * @description Сохраняет ссылки на используемые шины, информацию об источниках и инициализирует базовые обработчики
-     * @param {InitOpts} arg - объект со ссылками на внешние зависимости 
+     * @public
+     * @description Срабатывает при запросе системной службы фреймворка на отправку сообщения на WSC
+     * @param {string} _topic 
+     * @param {ClassBusMsg_S} _msg 
      */
-    async HandlerInit1({ ServicesState, SourcesState }) {
-        super.HandlerInit1(arguments);
-        if (this._BusList.lhpBus) {
-            /* future ver
-            this.AddComHandler('lhpBus', COM_PWSC_RECEIVE, this.#Receive.bind(this));
-            this.AddComHandler('lhpBus', COM_PWSC_SEND, this.#SendOnWSC.bind(this));
-            */
-        }
-    }
+    async HandlerEvents_proxywsc_send(_topic, _msg) {
+        const msg_to_plc = _msg.value[0];
+        // если запрос требует ответ, то его хэш сохраняется
+        if (_msg.metadata.demandRes)
+            this.#SaveMsgHash(_msg);
+        // TODO: LHPify
+        const lhp_msg = msg_to_plc;
+        // извлечение имени источника, на который требуется отправить сообщение
+        const source_name = _msg.arg[0];
 
+        this.EmitEvents_wsc_send({ value: [lhp_msg], arg: [source_name] });
+    }
     /**
      * @method 
-     * Вызывается при обработке события 'ws-receive'
-     * @param {string} _data - JSON-строка в LHP-формате
-     * @param {string} _sourceId - id/name отправителя
+     * @description Вызывается при обработке события 'proxywsc_msg_get', который инициируется WSC
+     * @param {string} _topic 
+     * @param {ClassBusMsg_S} _msg 
      */
-    #Receive(_data, _sourceId) {
-        const { com, arg } = LHP.Unpack(_data);
-        console.log(`DEBUG>> com ${com} arg ${arg}`);
-
-        if (com.endsWith('-raw')) {
-            this._BusList.sysBus.emit(`${_sourceId}-${com}`, arg[0]);
-        } else {
-            this._BusList.sysBus.emit(com, arg, _sourceId);
-            // this.SendMsg('lhpBus', { payload: { com, arg, source: _sourceId }});
-        }
+    HandlerEvents_proxywsc_get_msg(_topic, _msg) {
+        // извлечение "ядра" сообщения, составленного службой контроллера
+        // LHP.Unpack
+        const msg_from_plc = _msg.value[0];
+        const source_name = _msg.arg[0];
+        const hash = this.#GetMsgHash(msg_from_plc.com, source_name);
+        const msg = { 
+            dest: msg_from_plc.com.split('-')[0],
+            hash,
+            com: msg_from_plc.com,
+            arg: _msg.arg,      // arg = [sourceName] | sourceName извлекается из arg
+            value: [msg_from_plc]
+        };      
+        this.EmitMsg('lhpBus', msg_from_plc.com, msg);
     }
 
     /**
      * @method
-     * Отправляет сообщение в виде JSON-строки на WS Client
-     * @param {string} data сообщение 
+     * @public
+     * @description Отправляет сообщение на WSC
      */
-    #SendOnWSC(msg, sourceName) {
-        // TODO: актуализировать интерфейс взаимодействия
-        this._BusList.sysBus.emit(EVENT_PWSC_MSG_RETURN, JSON.stringify(msg), sourceName);
-        /**future ver 
-        // this.SendMsg('lhpBus', { topic: EVENT_PWSC_MSG_RETURN, payload: {
-        //     com: EVENT_PWSC_MSG_RETURN,
-        //     arg: [sourceName],
-        //     val: [JSON.stringify(msg)]
-        // }});   
-        */               
+    EmitEvents_wsc_send({ value=[], arg, dest }) {
+        const msg = this.CreateMsg({
+            com: COM_WSC_SEND,
+            value,
+            arg,
+            dest
+        });
+        return this.EmitMsg('lhpBus', COM_WSC_SEND, msg);         
+    }
+    /**
+     * @method
+     * @private
+     * @description Сохраняет хэш сообщения, требующего ответ
+     * @param {ClassBusMsg_S} _msg 
+     */
+    #SaveMsgHash(_msg) {
+        const { hash, resCom } = _msg.metadata; 
+        const sourceName = _msg.arg[0];
+        // создание ключа, по которому сохраняется запрос
+        const key = `${sourceName}_${resCom}`;
+        /* по ключу создается и вызывается асинхронная функция f1, которая возвращает f2
+           если f2 будет вызвана пользователем, то она вернет хэш запроса, выключит таймер ожидания и очистит свою позицию в списке.
+           Иначе выполнится таймаут вызванный в f1 и хэш уже нельзя будет получить.
+        */
+        this.#_RequestList[key] = (() => {
+            // callback очищающий ячейку списка
+            const clear_cb = (() => { delete this.#_RequestList[key]; }).bind(this);
+            // таймаут взводится на стандартное время TODO: оптимизировать время таймаута
+            const timeout = setTimeout(clear_cb, KEEP_ALIVE_HASH);
+            // возврат функции, вызов которой вернет хэш
+            return () => {
+                clearTimeout(timeout);
+                clear_cb();
+                return hash;
+            }
+        })();
+    }
+    /**
+     * @method
+     * @private
+     * @description возвращает хэш сообщения-запроса, ответ на который пришел
+     * @param {ClassBusMsg_S} _msg - сообщение-ответ
+     * @returns 
+     */
+    #GetMsgHash(_com, _sourceName) {
+        if (typeof _com === 'string' && typeof _sourceName === 'string') {
+            const key = `${_sourceName}_${_com}`;
+
+            const getHashFunc = this.#_RequestList[key];
+            return getHashFunc?.();
+        }
     }
 }
 
 module.exports = ClassProxyWSClient;
-
